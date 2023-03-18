475122732b50dcd227bc327b08b5749774f715e4
mohamed <2000gaber2000@gmail.com>
2023-03-18 18:55:40 +0200

    modifiy

---
+++ src/components/Navs/Nav.js
@@ -0,0 +1,14 @@
+
+import "./Nav.css"
+function Nav (){
+    return(
+      <div className="our-nav">
+         <div className="container">
+            <div className="nav-content">
+                <h1>welcome in our website</h1>
+            </div>
+         </div>
+      </div>
+    )
+}
+export default Nav
